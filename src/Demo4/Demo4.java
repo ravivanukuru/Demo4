854125705566247c9404c77bfe8fc5732a1fa1ab
vanukuru.ravikiran <vanukuru.ravikiran@NA-JM67CY3-L.cnf.prod.cnf.com>
2024-05-22 17:53:50 -0700

    demo4

---
+++ src/Demo4/Demo4.java
@@ -0,0 +1,12 @@
+package Demo4;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("print new line");
+		 System.out.println("print second new line");
+		 System.out.println("print Third new line");
+	}
+
+}
